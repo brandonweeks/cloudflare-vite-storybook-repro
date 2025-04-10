6414b37fc7a5a67505da19212fd573e78cf1641f
Brandon Weeks <me@brandonweeks.com>
2025-04-10 11:22:23 -0700

    add cloudflare vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import viteReact from "@vitejs/plugin-react";
 import { resolve } from "node:path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+  plugins: [
+    cloudflare(),
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": resolve(__dirname, "./src"),
     },
-  }
+  },
 });
